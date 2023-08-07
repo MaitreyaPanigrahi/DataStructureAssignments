8f6475c1c1276fa6bf10559033d032653079b515
MaitreyaPanigrahi <91478557+MaitreyaPanigrahi@users.noreply.github.com>
2023-08-07 17:37:59 +0530

    Add files via upload

---
+++ assign1.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+//Q1
+int x = 2;
+int y = 4;
+
+cout<<"Product of x & y is "<<x*y<<endl;
+
+
+//Q2
+int ASCII_of = 'U';
+cout<<ASCII_of<<endl;
+
+
+//Q3
+int length = 7;
+int breadth = 4;
+cout<<"Area of Rectangle is "<<length*breadth<<endl;
+
+//Q4
+int num = 4;
+int cubeOfNum = pow(num,3);
+cout<<"Cube of num is "<<cubeOfNum<<endl;
+
+
+//Q5
+cout<<"Size of char is "<<sizeof(char)<<endl;
+cout<<"Size of short is "<<sizeof(short)<<endl;
+cout<<"Size of int is "<<sizeof(int)<<endl;
+cout<<"Size of long is "<<sizeof(long)<<endl;
+cout<<"Size of long long is "<<sizeof(long long)<<endl;
+cout<<"Size of float is "<<sizeof(float)<<endl;
+cout<<"Size of double is "<<sizeof(double)<<endl;
+cout<<"Size of long double is "<<sizeof(long double)<<endl;
+cout<<"Size of bool is "<<sizeof(bool)<<endl;
+
+// Q6
+int a = 5;
+int b = 7;
+int c = b;
+b = a;
+a = c;
+cout<<"a: "<<a<<" "<<"b: "<<b<<endl;
+
+
+
+
+
+return 0;
+}
